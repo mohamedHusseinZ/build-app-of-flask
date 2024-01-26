f8c7482a5e1588ae003f941dff0a635e4d6fedf0
masud5201~ <your.email@masud.abdi@student.moringaschool.com>
2024-01-26 00:09:05 -0800

    done

--- server/app.py
+++ server/app.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/index')
+def index():
+    return '<h1>Welcome to my app!</h1>'
+
+@app.route('/user/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
